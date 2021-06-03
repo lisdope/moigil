b04454042f275b05252ef4ce4e3dca8fc9a92b6e
smj618 <smj618@naver.com>
2021-06-03 12:21:56 +0900

    모임 검색 기능추가

--- Moigil/src/main/java/com/springbook/biz/Meeting/MeetingController.java
+++ Moigil/src/main/java/com/springbook/biz/Meeting/MeetingController.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 
+
 @Controller
 public class MeetingController {
 	
@@ -28,6 +29,7 @@ public class MeetingController {
 		DAO.save(Meeting);
 	  return "getMeetingList.do";
 	}
+	
 	@RequestMapping("getMeeting.do") // 글보기
 	public String getMeeting(Model model,Meeting Meeting,@RequestParam(name="person",defaultValue = "0")Integer person) {
 		Meeting = DAO.findById(Meeting.getMeetingNo()).get();
@@ -36,8 +38,23 @@ public class MeetingController {
 		model.addAttribute("Meeting",Meeting);
 	  return "getMeeting.jsp";
 	}
+	
+	
 	@RequestMapping("getMeetingList.do")
-	public String getMeetingList(Model model,@RequestParam(name="PageNo",defaultValue = "0")Integer pNo){
+	public String getMeetingList(Model model,@RequestParam(name="PageNo",defaultValue = "0")Integer pNo, 
+			@RequestParam(name="searchCondition",defaultValue = "0")String searchCondition,
+			@RequestParam(name="searchKeyword",defaultValue = "0")String searchKeyword){
+		if (searchCondition.equals("TITLE") && searchKeyword != null) {
+			Pageable pageable = PageRequest.of(pNo, 10,Sort.Direction.ASC,"meetingNo");
+			Page<Meeting> page = DAO.findByMeetingtitle(searchKeyword, pageable);
+			model.addAttribute("page", page);
+			return "getMeetingList.jsp";
+		}else if (searchCondition.equals("CONTENT") && searchKeyword != null) {
+			Pageable pageable = PageRequest.of(pNo, 10,Sort.Direction.ASC,"meetingNo");
+			Page<Meeting> page = DAO.findByMeetingcontent(searchKeyword, pageable);
+			model.addAttribute("page", page);
+			return "getMeetingList.jsp";
+		}
 		Pageable pageable = PageRequest.of(pNo, 10,Sort.Direction.ASC,"meetingNo");
 		Page<Meeting> page = DAO.findAll(pageable);
 		model.addAttribute("page", page);
