42994b1f96937f204bc52f67e8d54952138974fa
starliveYJkim <starlivestarlove@gmail.com>
2021-06-15 15:34:11 +0900

    commit

---
+++ Moigil/src/main/java/com/springbook/biz/anonymous/AnonymousRepository.java
@@ -0,0 +1,12 @@
+package com.springbook.biz.anonymous;
+
+
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+
+public interface AnonymousRepository extends JpaRepository<Anonymous, Integer> {
+   
+}
